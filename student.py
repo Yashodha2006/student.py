74324ba798e2bd0bbe79db12482b86d5e0eebed5
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2026-01-05 15:12:37 +0530

    description

---
+++ student.py
@@ -0,0 +1,11 @@
+def student_details(name, student_id, department, marks):
+    return {
+        "Student Name": name,
+        "Student ID": student_id,
+        "Department": department,
+        "Marks": marks
+    }
+
+
+if __name__ == "__main__":
+    print(student_details("Alice", "S1001", "Computer Science", 85))
